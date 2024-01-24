0f48a562101811c0555578f6038c1eddac29093d
wawanneutron <wawanneutron1331@gmail.com>
2024-01-24 18:33:59 +0700

    add components reusable rating

--- 05-usepopcorn/src/index.js
+++ 05-usepopcorn/src/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import StarRating from './StarRating'
+// import './index.css'
+// import App from './App'
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+
+  return (
+    <>
+      <StarRating color="orange" size={68} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating size={28} color="red" className="test-classname" />
+    <StarRating messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+    <StarRating maxRating={10} color="purple" />
+    <Test />
   </React.StrictMode>
 )
