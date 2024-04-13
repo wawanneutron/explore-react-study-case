c75070be18616491be4fcef4ec664125066c3074
wawanneutron <wawanneutron1331@gmail.com>
2024-04-13 23:56:49 +0700

    error handling on form actions

--- 11-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 11-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Form, redirect } from 'react-router-dom'
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
 
 // https://uibakery.io/regex-library/phone-number
@@ -33,12 +33,17 @@ const fakeCart = [
 ]
 
 function CreateOrder() {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
+
+  const formErrors = useActionData()
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart
 
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>Ready to order? Let&apos;s go!</h2>
 
       <Form method="POST">
         <div>
@@ -51,6 +56,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <i>{formErrors.phone}</i>}
         </div>
 
         <div>
@@ -73,7 +79,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? 'Placing order...' : 'Order now'}
+          </button>
         </div>
       </Form>
     </div>
@@ -90,6 +98,15 @@ export async function Action({ request }) {
     priority: data.priority === 'on'
   }
 
+  const errors = {}
+
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      'Please give us your correct phone number. We might need it to contact you.'
+
+  if (Object.keys(errors).length > 0) return errors
+
+  // If everything is okey, create new order and redirect
   const newOrder = await createOrder(order)
 
   return redirect(`/order/${newOrder.id}`)
