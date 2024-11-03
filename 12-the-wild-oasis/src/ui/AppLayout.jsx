b19adf485b6a1e3995bcda73eb3dd9233b93bcdb
wawanneutron <wawanneutron1331@gmail.com>
2024-11-03 13:36:19 +0700

    prettier formatter

--- 12-the-wild-oasis/src/ui/AppLayout.jsx
+++ 12-the-wild-oasis/src/ui/AppLayout.jsx
@@ -1,20 +1,20 @@
-import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
-import Header from "./Header";
-import styled from "styled-components";
+import { Outlet } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import Header from './Header'
+import styled from 'styled-components'
 
 const StyledAppLayout = styled.div`
   display: grid;
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
   height: 100vh;
-`;
+`
 
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
   overflow: scroll;
-`;
+`
 
 const Container = styled.div`
   max-width: 120rem;
@@ -22,7 +22,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 3.2rem;
-`;
+`
 
 function AppLayout() {
   return (
@@ -35,7 +35,7 @@ function AppLayout() {
         </Container>
       </Main>
     </StyledAppLayout>
-  );
+  )
 }
 
-export default AppLayout;
+export default AppLayout
