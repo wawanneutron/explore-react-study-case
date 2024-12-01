8974abb647220a9cf04ef1b8de6b1a88a313ce54
wawanneutron <wawanneutron1331@gmail.com>
2024-12-01 16:06:24 +0700

    protected route

--- 12-the-wild-oasis/src/ui/ProtectedRoute.jsx
+++ 12-the-wild-oasis/src/ui/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { redirect, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useUser } from '../features/authentication/useUser'
 import Spinner from './Spinner'
 import styled from 'styled-components'
