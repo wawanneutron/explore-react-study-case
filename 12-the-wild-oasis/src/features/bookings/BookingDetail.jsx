125f47524a3bc57900db7e88aaad20f6e63e6a3e
wawanneutron <wawanneutron1331@gmail.com>
2025-01-12 10:20:31 +0700

    dark mode

--- 12-the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ 12-the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -1,53 +1,53 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-import BookingDataBox from "./BookingDataBox";
-import Row from "../../ui/Row";
-import Heading from "../../ui/Heading";
-import Tag from "../../ui/Tag";
-import ButtonGroup from "../../ui/ButtonGroup";
-import Button from "../../ui/Button";
-import ButtonText from "../../ui/ButtonText";
+import BookingDataBox from './BookingDataBox'
+import Row from '../../ui/Row'
+import Heading from '../../ui/Heading'
+import Tag from '../../ui/Tag'
+import ButtonGroup from '../../ui/ButtonGroup'
+import Button from '../../ui/Button'
+import ButtonText from '../../ui/ButtonText'
 
-import { useMoveBack } from "../../hooks/useMoveBack";
-import { useBooking } from "./useBooking";
-import Spinner from "../../ui/Spinner";
-import { useNavigate } from "react-router-dom";
-import { HiArrowUpOnSquare } from "react-icons/hi2";
-import { useCheckout } from "../check-in-out/useCheckout";
-import Modal from "../../ui/Modal";
-import ConfirmDelete from "../../ui/ConfirmDelete";
-import { useDeleteBooking } from "./useDeleteBooking";
+import { useMoveBack } from '../../hooks/useMoveBack'
+import { useBooking } from './useBooking'
+import Spinner from '../../ui/Spinner'
+import { useNavigate } from 'react-router-dom'
+import { HiArrowUpOnSquare } from 'react-icons/hi2'
+import { useCheckout } from '../check-in-out/useCheckout'
+import Modal from '../../ui/Modal'
+import ConfirmDelete from '../../ui/ConfirmDelete'
+import { useDeleteBooking } from './useDeleteBooking'
 
 const HeadingGroup = styled.div`
   display: flex;
   gap: 2.4rem;
   align-items: center;
-`;
+`
 
 function BookingDetail() {
-  const { booking, isLoading } = useBooking();
-  const { checkout, isCheckingOut } = useCheckout();
-  const { deleteBooking, isDeleting } = useDeleteBooking();
+  const { booking, isLoading } = useBooking()
+  const { checkout, isCheckingOut } = useCheckout()
+  const { deleteBooking, isDeleting } = useDeleteBooking()
 
-  const moveBack = useMoveBack();
-  const navigate = useNavigate();
+  const moveBack = useMoveBack()
+  const navigate = useNavigate()
 
-  if (isLoading) return <Spinner />;
+  if (isLoading) return <Spinner />
 
-  const { status, id: bookingId } = booking;
+  const { status, id: bookingId } = booking
 
   const statusToTagName = {
-    unconfirmed: "blue",
-    "checked-in": "green",
-    "checked-out": "silver",
-  };
+    unconfirmed: 'blue',
+    'checked-in': 'green',
+    'checked-out': 'silver',
+  }
 
   return (
     <>
       <Row type="horizontal">
         <HeadingGroup>
           <Heading as="h1">Booking #{bookingId}</Heading>
-          <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
+          <Tag type={statusToTagName[status]}>{status.replace('-', ' ')}</Tag>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
@@ -55,13 +55,13 @@ function BookingDetail() {
       <BookingDataBox booking={booking} />
 
       <ButtonGroup>
-        {status === "unconfirmed" && (
+        {status === 'unconfirmed' && (
           <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
             Check in
           </Button>
         )}
 
-        {status === "checked-in" && (
+        {status === 'checked-in' && (
           <Button
             icon={<HiArrowUpOnSquare />}
             onClick={() => checkout(bookingId)}
@@ -94,7 +94,7 @@ function BookingDetail() {
         </Button>
       </ButtonGroup>
     </>
-  );
+  )
 }
 
-export default BookingDetail;
+export default BookingDetail
--- 12-the-wild-oasis/src/ui/HeaderMenu.jsx
+++ 12-the-wild-oasis/src/ui/HeaderMenu.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import Logout from '../features/authentication/Logout'
+import DarkModeToggle from './DarkModeToggle'
 import ButtonIcon from './ButtonIcon'
 import { HiOutlineUser } from 'react-icons/hi2'
 import { useNavigate } from 'react-router-dom'
@@ -18,6 +19,9 @@ function HeaderMenu() {
         <ButtonIcon onClick={() => navigate('/account')}>
           <HiOutlineUser />
         </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
       <li>
         <Logout />
