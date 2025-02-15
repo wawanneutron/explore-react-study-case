70ca70b8566f1a33ab773c313f1d5677adb5e295
wawanneutron <wawanneutron1331@gmail.com>
2025-02-15 12:33:22 +0700

    chart dashvboard - today activity

--- 12-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 12-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Spinner from '../../ui/Spinner'
 import Stats from './Stats'
 import SalesChart from './SalesChart'
 import DurationChart from './DurationChart'
+import TodayActivity from '../check-in-out/TodayActivity'
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -16,12 +17,7 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading: isLoadingBooking } = useRecentBookings()
-  const {
-    stays,
-    confirmedStays,
-    isLoading: isLoadingStay,
-    numDays,
-  } = useRecentStays()
+  const { confirmedStays, isLoading: isLoadingStay, numDays } = useRecentStays()
   const { cabins, isLoading: isLoadingCabin } = useCabins()
 
   if (isLoadingBooking || isLoadingStay || isLoadingCabin) return <Spinner />
@@ -34,7 +30,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Statistics</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
